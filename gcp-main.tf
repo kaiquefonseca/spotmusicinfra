fad977c23b8fa20955f140921e949f155746f9e1
JVennom <105567595+JVennom@users.noreply.github.com>
2022-07-02 15:20:34 -0300

    Construção do DB

--- gcp-main.tf
+++ gcp-main.tf
@@ -42,3 +42,19 @@ resource "google_cloud_run_service" "spotback" {
     latest_revision = true
   }
 }
+resource "google_sql_database" "spotmusicdb" {
+  name     = "spotmusicdb"
+  instance = google_sql_database_instance.instance.name
+}
+
+# See versions at https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/sql_database_instance#database_version
+resource "google_sql_database_instance" "main" {
+  name             = "spotmusic"
+  region           = "us-central1"
+  database_version = "MYSQL_8_0"
+  settings {
+    tier = "db-f1-micro"
+  }
+
+  deletion_protection  = "true"
+}
